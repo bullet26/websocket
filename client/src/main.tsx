763481526d5f8a6076badb4d5388296d5fafa8ae
Julia <bullet.jul@gmail.com>
2024-01-11 14:13:28 +0200

    addflip card

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -3,6 +3,23 @@ import { ContextProvider } from 'store'
 import { App } from './App'
 import './index.scss'
 
+// declare global {
+//   interface Window {
+//     initPlugin: (id: string) => void
+//   }
+// }
+
+// window.initPlugin = (id: string) => {
+//   const root = ReactDOM.createRoot(document.getElementById(id) as HTMLElement)
+//   root.render(
+//     <ContextProvider>
+//       <App />
+//     </ContextProvider>,
+//   )
+// }
+
+// standard
+
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <ContextProvider>
     <App />
