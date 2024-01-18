623663d113db2c201d46d367e5f9254fcd23a205
Julia <bullet.jul@gmail.com>
2024-01-18 12:07:11 +0200

    upd for bubble

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -3,25 +3,25 @@ import { ContextProvider } from 'store'
 import { App } from './App'
 import './index.scss'
 
-// declare global {
-//   interface Window {
-//     initPlugin: (id: string) => void
-//   }
-// }
+declare global {
+  interface Window {
+    initPlugin: (id: string) => void
+  }
+}
 
-// window.initPlugin = (id: string) => {
-//   const root = ReactDOM.createRoot(document.getElementById(id) as HTMLElement)
-//   root.render(
-//     <ContextProvider>
-//       <App />
-//     </ContextProvider>,
-//   )
-// }
+window.initPlugin = (id: string) => {
+  const root = ReactDOM.createRoot(document.getElementById(id) as HTMLElement)
+  root.render(
+    <ContextProvider>
+      <App />
+    </ContextProvider>,
+  )
+}
 
 // standard
 
-ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
-  <ContextProvider>
-    <App />
-  </ContextProvider>,
-)
+// ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
+//   <ContextProvider>
+//     <App />
+//   </ContextProvider>,
+// )
